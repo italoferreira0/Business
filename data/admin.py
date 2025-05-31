8c8004bc38ae72298d9f23c66844997755338e7f
Italo Ferreira <italo.ferreira@nposervices.com>
2025-05-30 21:53:22 -0300

    first commit

---
+++ data/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from data.models import MaioresDividendos
+
+admin.site.register(MaioresDividendos)
---
+++ data/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class MaioresDividendos(models.Model):
+    codigo = models.CharField(max_length=50)
+    dividendo_atual = models.DecimalField(max_digits=5, decimal_places=2)
+    dividendo_medio = models.DecimalField(max_digits=5, decimal_places=2)
+    p_l = models.DecimalField(max_digits=6, decimal_places=2)
+    p_vp = models.DecimalField(max_digits=6, decimal_places=2)
+    margem_liquida = models.DecimalField(max_digits=5, decimal_places=2)
+    valor_mercado = models.DecimalField(max_digits=15, decimal_places=2)
+    setor = models.CharField(max_length=50)
+    data = models.DateField()
+
+    class Meta:
+        verbose_name = 'Maior Dividendo'
+        verbose_name_plural = 'Maiores Dividendos'
+        ordering = ['-setor']
