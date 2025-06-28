3b624ea216e8e7cdfd12f36f3b9e63ed51e09794
Italo Ferreira <italo.ferreira@nposervices.com>
2025-06-28 15:22:00 -0300

    Atualização na Api

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import MaioresDividendosView, MaioresLucrosView, HomePageView
+from .views import  HomePageView, MaioresDividendosView, Top10MaioresDividendosView, Top10MaioresDividendosMedioView , MaioresLucrosView,Top10MaioresLucrosView
 
 urlpatterns = [
     path('maiores-dividendos/', MaioresDividendosView.as_view(), name='maiores-dividendos'),
+    path('top10-dividendos/', Top10MaioresDividendosView.as_view(), name='top10-dividendos'),
+    path('top10-dividendos-medio/', Top10MaioresDividendosMedioView.as_view(), name='top10-dividendos-medio'),
+
     path('maiores-lucros/', MaioresLucrosView.as_view(), name='maiores-lucros'),
+    path('top10-maiores-lucros/', Top10MaioresLucrosView.as_view(), name='top10-maiores-lucros'),
     path('', HomePageView.as_view(), name='api'),
 ]
--- api/views.py
+++ api/views.py
@@ -10,18 +10,40 @@ class HomePageView(APIView):
         return Response({
             "Endpoints": {
                 "Maiores Dividendos": request.build_absolute_uri('/api/maiores-dividendos/'),
-                "Maiores Lucros": request.build_absolute_uri('/api/maiores-lucros/')
+                "Top 10 Maiores Dividendos": request.build_absolute_uri('/api/top10-dividendos/'),
+                "Top 10 Maiores Dividendos Médio": request.build_absolute_uri('/api/top10-dividendos-medio/'),
+                
+                "Maiores Lucros": request.build_absolute_uri('/api/maiores-lucros/'),
+                "Top 10 Maiores Lucros": request.build_absolute_uri('/api/top10-maiores-lucros/')
             }
         })
 
 class MaioresDividendosView(APIView):
     def get(self, request):
-        maioresDividendos = MaioresDividendos.objects.order_by('-setor')
+        maioresDividendos = MaioresDividendos.objects.order_by('-dividendo_atual')
         serializer = MaioresDividendosSerializer(maioresDividendos, many=True)
         return Response(serializer.data)
-    
+
+class Top10MaioresDividendosView(APIView):
+    def get(self, request):
+        top_dividendo = MaioresDividendos.objects.order_by('-dividendo_atual')[:10]
+        serializer = MaioresDividendosSerializer(top_dividendo, many=True)
+        return Response(serializer.data)
+
+class Top10MaioresDividendosMedioView(APIView):
+    def get(self, request):
+        top_dividendo = MaioresDividendos.objects.order_by('-dividendo_medio')[:10]
+        serializer = MaioresDividendosSerializer(top_dividendo, many=True)
+        return Response(serializer.data)
+
 class MaioresLucrosView(APIView):
     def get(self, request):
         maioresLucros = MaioresLucros.objects.order_by('-setor')
         serializer = MaioresLucrosSerializer(maioresLucros, many=True)
+        return Response(serializer.data)
+
+class Top10MaioresLucrosView(APIView):
+    def get(self, request):
+        top_maioresLucros = MaioresLucros.objects.order_by('-lucro')[:10]
+        serializer = MaioresLucrosSerializer(top_maioresLucros, many=True)
         return Response(serializer.data)
