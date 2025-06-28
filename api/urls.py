fe8e42b3cb09f8b6012f7fadafbd4a5ad9de1ef5
Italo Ferreira <italo.ferreira@nposervices.com>
2025-06-28 13:57:50 -0300

    add pagina inicial Api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MaioresDividendosView, MaioresLucrosView
+from .views import MaioresDividendosView, MaioresLucrosView, HomePageView
 
 urlpatterns = [
     path('maiores-dividendos/', MaioresDividendosView.as_view(), name='maiores-dividendos'),
     path('maiores-lucros/', MaioresLucrosView.as_view(), name='maiores-lucros'),
+    path('', HomePageView.as_view(), name='api'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.urls import reverse
+from rest_framework.request import Request
 from data.models import MaioresDividendos, MaioresLucros
 from .serializer import MaioresDividendosSerializer, MaioresLucrosSerializer
+
+class HomePageView(APIView):
+    def get(self, request):
+        return Response({
+            "Endpoints": {
+                "Maiores Dividendos": request.build_absolute_uri('/api/maiores-dividendos/'),
+                "Maiores Lucros": request.build_absolute_uri('/api/maiores-lucros/')
+            }
+        })
 
 class MaioresDividendosView(APIView):
     def get(self, request):
